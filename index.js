761dbdc23d1d218dae24c946c022d01ee94d5dd5
Kalonde Julius <58883291+Kalonde-Julius@users.noreply.github.com>
2021-05-23 13:19:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as CustomDrawer} from './app.json';
+
+AppRegistry.registerComponent(CustomDrawer, () => App);
